ac92c6bac723479c54cd170aca0bdb550f792c64
Amit Shrivastava <amit.shrivastava@idmworks.com>
2019-04-29 17:18:59 +0530

    Initial commit

---
+++ user-api/src/main/java/com/learning/app/config/AppConfig.java
@@ -0,0 +1,7 @@
+package com.learning.app.config;
+
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+
+@EnableGlobalMethodSecurity(prePostEnabled = true)
+public class AppConfig {
+}

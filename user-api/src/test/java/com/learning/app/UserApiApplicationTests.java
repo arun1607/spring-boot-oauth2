8fec270a778b7cc96949d8c106a0827800ed99a6
Amit Shrivastava <arun160782@gmai.com>
2019-05-10 16:43:16 +0530

    Added test cases

--- user-api/src/test/java/com/learning/app/UserApiApplicationTests.java
+++ user-api/src/test/java/com/learning/app/UserApiApplicationTests.java
@@ -1,16 +1,43 @@
 package com.learning.app;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.web.FilterChainProxy;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
-@SpringBootTest
+@WebAppConfiguration
+@SpringBootTest(classes = UserApiApplication.class)
 public class UserApiApplicationTests {
 
-	@Test
-	public void contextLoads() {
-	}
+    @Autowired
+    private WebApplicationContext wac;
 
+    @Autowired
+    private FilterChainProxy springSecurityFilterChain;
+
+    private MockMvc mockMvc;
+
+    @Before
+    public void setup() {
+        this.mockMvc = MockMvcBuilders.webAppContextSetup(this.wac)
+                .addFilter(springSecurityFilterChain).build();
+    }
+
+    @Test
+    public void givenNoToken_whenGetSecureRequest_thenUnauthorized() throws Exception {
+        mockMvc.perform(get("/me")
+                .param("email", "abc@hgc.com"))
+                .andExpect(status().isUnauthorized());
+    }
 }
